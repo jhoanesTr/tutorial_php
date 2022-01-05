4121e1f5b8fd3d2125234de7c640663ed7eaa776
jhoanesTr <89508333+jhoanesTr@users.noreply.github.com>
2022-01-05 14:40:04 +0100

    Add files via upload

---
+++ php/mi_primer_php/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Hola mundo";
+//Esa fue la primera frase
+
+echo "Ultima frase";
+//Aqui termina echo
+///////////////////////////
+
+$personas = 20;//Numero "entero o INT" de personas en el trabajo
+if (is_int($personas)) {
+    echo $personas;
+}//Si la "variable personas" es un entero: imprimir 
+
+$esto_es_rojo = TRUE;//Booleano en modo "VERDADERO" "1"
+$esto_es_amarillo = FALSE;//Booleano en modo "VERDADERO" "0"
